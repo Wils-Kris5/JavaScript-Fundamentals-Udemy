165cca2f519534ec312401a5c2065e09c03c4897
Kristen Wilson <Kristen@Kristens-MacBook-Air.local>
2022-02-01 16:01:56 -0600

    left off at data types

---
+++ assignments.js
@@ -0,0 +1,29 @@
+//LECTURE: Values and Variables
+// 1. Declare variables called 'country', 'continent' and 'population' and
+// assign their values according to your own country (population in millions)
+// 2. Log their values to the console
+let country = "United States";
+
+console.log(country);
+
+let continent = "North America";
+
+console.log(continent);
+
+let population = 329.5;
+
+console.log(population);
+
+// LECTURE: Data Types
+// 1. Declare a variable called'isIsland'and set its value according to your country. The variable should hold a Boolean value. Also declare a variable 'language', but don't assign it any value yet
+// 2. Log thet ypesof'isIsland','population','country'and'language' to the console
+let isIsland = false;
+
+let language;
+
+console.log(isIsland);
+console.log(language);
+console.log(typeof country);
+console.log(typeof population);
+console.log(typeof isIsland);
+console.log(typeof language);
---
+++ script.js
@@ -0,0 +1,40 @@
+// let js = 'amazing';
+// if (js === 'amazing') alert('JavaScript is fun!');
+
+// console.log(40 + 8 + 23 - 10);
+//a value is a piece of data, the smallest unit of information, we can have different values.
+//  variable      value
+// let firstName = "Kristen"
+//imagine a variable like a box, a box can hold some object, and we can write a label to describe whats in it and can find the object later using that label. 
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+
+// Data Types 
+//declared new variable with the true value
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Kristen');
+
+//assigning new values to variables 
+//we do not re-write 'let' (that is only for the first time) we are changing the content of the box (javascriptIsFun) which at first had the value of true inside, and now replacing it with the value string of yes.
+
+javascriptIsFun = 'Yes!';
+console.log(typeof javascriptIsFun);
+
+//undefined data types:
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+//error in typeof operator 
+// console.log(typeof null);
+//object < this is a bug or error null is not an object.
